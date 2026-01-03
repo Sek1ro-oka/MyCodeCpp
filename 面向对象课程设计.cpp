222f2fcfb0b32190ebce3f381495658ff752a976
Sek1ro-oka <LIUGUANYI0721@gmail.com>
2026-01-03 22:20:49 +0800

    Update 面向对象课程设计.cpp

--- 面向对象课程设计.cpp
+++ 面向对象课程设计.cpp
@@ -1,93 +1,286 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iomanip>
-#include <fstream>
-#include <stack>
-#include <queue>
-#include <cmath>
-#include <unordered_map>
-#include "client.h"
+#include <conio.h>
+#include <windows.h>
+#include "server.h"
 
 using namespace std;
 
-typedef string client_string;
-typedef double client_double;
-typedef int client_int;
-typedef string entrustment_string;
-typedef double entrustment_double;
-
-// 服务端代码实现
-class server
+/* ================== 页面枚举 ================== */
+enum class Page
 {
-public:
-    server() = default;
-    // 拷贝构造
-    server(const server &other) : idMap_users(other.idMap_users), idMap_events(other.idMap_events) {}
-    // 移动语义快速拷贝大对象
-    server(server &&other) noexcept : idMap_users(std::move(other.idMap_users)), idMap_events(std::move(other.idMap_events)) {}
+    LOGIN,
+    REGISTER,
+    ENTRUST_LIST,
+    MY_DISPATCH,
+    MY_ACCEPT,
+    USER_INFO,
+    USER_PUBLISH,
+    ENTRUST_DETAIL
+};
 
-    // 显示所有用户信息
-    void show_all_users()
+void clear()
+{
+    system("cls");
+}
+
+/* ================== 委托详情 ================== */
+void drawEntrustDetail(const entrustment *e)
+{
+    cout << "【委托详情】\n\n";
+    cout << "标题：" << e->getName() << endl;
+    cout << "编号：" << e->getId() << endl;
+    cout << "时间：" << e->getTime() << endl;
+    cout << "地点：" << e->getLocation() << endl;
+    cout << "内容：" << e->getContent() << endl;
+    cout << "报酬：" << e->getProfit() << endl;
+
+    cout << "\n----- 评论区 -----\n";
+    for (auto &c : e->getComments())
+        cout << "- " << c << endl;
+}
+
+/* ================== 主函数 ================== */
+int main()
+{
+    server srv;
+
+    // 预置一个测试用户
+    srv.register_client("张三", "男", "123", "123", "a@qq.com");
+
+    Page page = Page::LOGIN;
+    client *current_user = nullptr;
+    const entrustment *current = nullptr;
+
+    int loginCursor = 0; // 0=登录 1=注册 2=退出
+    int cursor = 0;
+    string phone, password;
+
+    while (true)
     {
-        if (idMap_users.size() <= 0)
-            return;
-        for (auto user : idMap_users)
+        clear();
+
+        /* ================== 登录界面 ================== */
+        if (page == Page::LOGIN)
         {
-            user.second.showInformation();
-            user.second.show_dispatch_history();
-            user.second.show_accpect_history();
+            cout << "====== 欢迎使用委托系统 ======\n\n";
+
+            const char *menu[] = {
+                "登录",
+                "注册",
+                "退出"};
+
+            for (int i = 0; i < 3; ++i)
+            {
+                cout << (i == loginCursor ? "> " : "  ")
+                     << menu[i] << endl;
+            }
+
+            cout << "\nW/S 选择  Enter 确认\n";
+        }
+
+        /* ================== 注册界面 ================== */
+        else if (page == Page::REGISTER)
+        {
+            string name, gender, email;
+            cout << "====== 注册 ======\n\n";
+            cout << "姓名：";
+            cin >> name;
+            cout << "性别：";
+            cin >> gender;
+            cout << "手机号：";
+            cin >> phone;
+            cout << "密码：";
+            cin >> password;
+            cout << "邮箱：";
+            cin >> email;
+
+            srv.register_client(name, gender, phone, password, email);
+            cout << "\n注册成功，按任意键返回登录...";
+            _getch();
+            page = Page::LOGIN;
+        }
+
+        /* ================== 委托列表 ================== */
+        else if (page == Page::ENTRUST_LIST)
+        {
+            cout << "【委托列表】\n\n";
+            int idx = 0;
+            for (auto &p : srv.getAllEntrustments())
+            {
+                cout << (idx == cursor ? "> " : "  ")
+                     << p.second.getName() << endl;
+                idx++;
+            }
+
+            cout << "\nA/D 切页  W/S 移动  Enter 查看  ESC 退出\n";
+        }
+
+        /* ================== 我发布 ================== */
+        else if (page == Page::MY_DISPATCH)
+        {
+            cout << "【我发布的委托】\n\n";
+            const auto &list = current_user->getDispatchHistory();
+            for (int i = 0; i < (int)list.size(); ++i)
+                cout << (i == cursor ? "> " : "  ")
+                     << list[i].getName() << endl;
+        }
+
+        /* ================== 我承接 ================== */
+        else if (page == Page::MY_ACCEPT)
+        {
+            cout << "【我承接的委托】\n\n";
+            const auto &list = current_user->getAcceptHistory();
+            for (int i = 0; i < (int)list.size(); ++i)
+                cout << (i == cursor ? "> " : "  ")
+                     << list[i].getName() << endl;
+        }
+
+        /* ================== 个人信息 ================== */
+        else if (page == Page::USER_INFO)
+        {
+            cout << "【个人信息】\n\n";
+            cout << "姓名：" << current_user->getName() << endl;
+            cout << "性别：" << current_user->getGender() << endl;
+            cout << "手机：" << current_user->getPhone() << endl;
+            cout << "邮箱：" << current_user->getEmail() << endl;
+            cout << "星级：" << current_user->getStar()
+                 << "（" << current_user->getStarLevel() << "）\n";
+            cout << "信誉分：" << current_user->getCreait() << endl;
+
+            cout << "\n[Enter] 发布委托  [ESC] 返回\n";
+        }
+
+        /* ================== 发布委托 ================== */
+        else if (page == Page::USER_PUBLISH)
+        {
+            string n, t, l, c;
+            double p;
+
+            cout << "【发布委托】\n\n";
+            cout << "标题：";
+            cin >> n;
+            cout << "时间：";
+            cin >> t;
+            cout << "地点：";
+            cin >> l;
+            cout << "内容：";
+            cin >> c;
+            cout << "报酬：";
+            cin >> p;
+
+            srv.dispatch(current_user->getID(), n, t, l, c, p);
+
+            cout << "\n发布成功，按任意键返回...";
+            _getch();
+            page = Page::MY_DISPATCH;
+        }
+
+        /* ================== 委托详情 ================== */
+        else if (page == Page::ENTRUST_DETAIL && current)
+        {
+            drawEntrustDetail(current);
+            cout << "\n[Enter] 接受委托  [ESC] 返回\n";
+        }
+
+        /* ================== 键盘处理 ================== */
+        char ch = _getch();
+        if (page == Page::LOGIN)
+        {
+            if (ch == 'w')
+                loginCursor = max(0, loginCursor - 1);
+            if (ch == 's')
+                loginCursor = min(2, loginCursor + 1);
+        }
+
+        if (ch == 27) // ESC
+        {
+            if (page == Page::ENTRUST_DETAIL)
+                page = Page::ENTRUST_LIST;
+            else if (page == Page::USER_PUBLISH)
+                page = Page::USER_INFO;
+            else
+                break;
+        }
+
+        if (ch == 'w')
+            cursor = max(0, cursor - 1);
+
+        if (ch == 's')
+            cursor++;
+
+        if (ch == 'a')
+        {
+            page = Page((int(page) + 6) % 7);
+            cursor = 0;
+        }
+
+        if (ch == 'd')
+        {
+            page = Page((int(page) + 1) % 7);
+            cursor = 0;
+        }
+
+        if (ch == 13) // Enter
+        {
+            if (page == Page::LOGIN)
+            {
+                if (loginCursor == 0) // 登录
+                {
+                    clear();
+                    cout << "====== 登录 ======\n\n";
+                    cout << "手机号：";
+                    cin >> phone;
+                    cout << "密码：";
+                    cin >> password;
+
+                    current_user = srv.login(phone, password);
+                    if (current_user)
+                    {
+                        page = Page::ENTRUST_LIST;
+                        cursor = 0;
+                    }
+                    else
+                    {
+                        cout << "\n登录失败，按任意键返回...";
+                        _getch();
+                    }
+                }
+                else if (loginCursor == 1) // 注册
+                {
+                    page = Page::REGISTER;
+                }
+                else // 退出
+                {
+                    return 0;
+                }
+            }
+
+            else if (page == Page::ENTRUST_LIST)
+            {
+                int idx = 0;
+                for (auto &p : srv.getAllEntrustments())
+                {
+                    if (idx == cursor)
+                    {
+                        current = &p.second;
+                        page = Page::ENTRUST_DETAIL;
+                        break;
+                    }
+                    idx++;
+                }
+            }
+            else if (page == Page::ENTRUST_DETAIL && current)
+            {
+                srv.accept_clint(current_user->getID(), current->getId());
+                page = Page::MY_ACCEPT;
+                cursor = 0;
+            }
+            else if (page == Page::USER_INFO)
+            {
+                page = Page::USER_PUBLISH;
+            }
         }
     }
 
-    // 用户注册
-    bool register_client(client_string name, client_string gender, client_string phone, client_string password, client_string email)
-    {
-        client new_user(name, gender, phone, password, email);
-        idMap_users[new_user.getID()] = std::move(new_user);
-        return true;
-    }
-
-    // 创建管理员账户
-    bool creat_administrator_account(client_string name, client_string gender, client_string phone, client_string password, client_string email)
-    {
-        client administrator(name, gender, phone, password, email, true);
-        idMap_users[administrator.getID()] = std::move(administrator);
-        return true;
-    }
-
-    // 通过序列号来查找委托人
-    client *find_client(client_string &id)
-    {
-        auto it = idMap_users.find(id);
-        if (it != idMap_users.end())
-            return &(it->second);
-        return nullptr;
-    }
-
-    // 发布委托
-    void dispatch(client_string &Boss_id, entrustment_string name, entrustment_string time, entrustment_string location, entrustment_string content, entrustment_double profit)
-    {
-        // 委托人
-        entrustment event = (*find_client(Boss_id)).Creat_entrustment(name, time, location, content, profit);
-        idMap_events[event.getId()] = std::move(event);
-    }
-
-    // 账户注销
-    void delete_account(client_string &id)
-    {
-        idMap_users.erase(id);
-    }
-
-private:
-    unordered_map<client_string, client> idMap_users;
-    unordered_map<entrustment_string, entrustment> idMap_events;
-};
-int main()
-{
-    server server1;
-    server1.register_client("张三", "沃尔玛购物袋", "1242685585", "123456", "125413623@qq.com");
-    server1.creat_administrator_account("张羡光", "无人直升机", "1242685585", "123456", "125413623@qq.com");
-    server1.show_all_users();
     return 0;
 }
