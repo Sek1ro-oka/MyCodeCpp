163c996f4d5b7bb7aa2b195e6bb5703dd04e92c8
Sek1ro-oka <LIUGUANYI0721@gmail.com>
2026-01-05 12:38:02 +0800

    Update 面向对象课程设计.cpp

--- 面向对象课程设计.cpp
+++ 面向对象课程设计.cpp
@@ -1,4 +1,5 @@
 #include <conio.h>
+#include <limits>
 #include "server.h"
 
 using namespace std;
@@ -6,14 +7,17 @@ using namespace std;
 /* ================== 页面枚举 ================== */
 enum class Page
 {
-    LOGIN,         // 登录
-    REGISTER,      // 注册
-    ENTRUST_LIST,  // 委托列表
-    MY_DISPATCH,   // 我发布的委托
-    MY_ACCEPT,     // 我承接的委托
-    USER_INFO,     // 用户个人信息
-    USER_PUBLISH,  // 发布委托
-    ENTRUST_DETAIL // 委托详情
+    LOGIN,          // 登录
+    REGISTER,       // 注册
+    ENTRUST_LIST,   // 委托列表
+    MY_DISPATCH,    // 我发布的委托
+    MY_ACCEPT,      // 我承接的委托
+    USER_INFO,      // 用户个人信息
+    USER_PUBLISH,   // 发布委托
+    ENTRUST_DETAIL, // 委托详情
+    ACCEPT_DETAIL,  // ? 新增：承接委托详情
+    USER_EDIT       // 编辑个人页面
+
 };
 
 /* ================== 可切换页面表 ================== */
@@ -48,14 +52,25 @@ void drawEntrustDetail(const entrustment *e)
 int main()
 {
     server srv;
-    // 存档
+    // 读档
     srv.loadFromFile();
+
+    //  预设管理员（只在第一次）
+    if (srv.getAllUsers().empty())
+    {
+        srv.creat_administrator_account(
+            "管理员",
+            "男",
+            "admin",
+            "admin",
+            "admin@qq.com");
+    }
 
     Page page = Page::LOGIN;        // 最开始的登录界面
     client *current_user = nullptr; // 登录用户
     const entrustment *current = nullptr;
 
-    int loginCursor = 0; // 0=登录 1=注册 2=退出
+    int loginCursor = 0; // 0=用户登录 1=管理员登录 2=注册 3=退出
     int cursor = 0;      // 光标
     string phone, password;
 
@@ -69,7 +84,8 @@ int main()
             cout << "====== 帮个忙 ======\n\n";
             // 菜单页面
             const char *menu[] = {
-                "登录",
+                "普通用户登录",
+                "管理员登录",
                 "注册",
                 "退出"};
 
@@ -128,7 +144,7 @@ int main()
                 cout << (i == cursor ? "> " : "  ")
                      << list[i].getName() << endl;
 
-            cout << "\nA/D 切页  W/S 移动  Enter 查看  ESC 退出\n";
+            cout << "\nA/D 切页  W/S 移动  [R] 删除委托  Enter 查看  ESC 返回\n";
         }
 
         /* ================== 我承接 ================== */
@@ -155,7 +171,70 @@ int main()
                  << "（" << current_user->getStarLevel() << "）\n";
             cout << "信誉分：" << current_user->getCreait() << endl;
 
-            cout << "\n[Enter] 发布委托  [ESC] 返回\n";
+            cout << "\n[1] 修改个人信息  [Enter] 发布委托  [ESC] 返回\n";
+        }
+
+        /* ================== 编辑个人信息 ================== */
+        else if (page == Page::USER_EDIT)
+        {
+            clear();
+            cout << "【修改个人信息】\n\n";
+            cout << "1. 姓名\n";
+            cout << "2. 性别\n";
+            cout << "3. 手机\n";
+            cout << "4. 邮箱\n";
+            cout << "5. 密码\n";
+            cout << "ESC 返回\n\n";
+
+            char ch = _getch();
+            string v;
+
+            if (ch == '1')
+            {
+                cout << "\n新姓名：";
+                cin >> v;
+                current_user->setName(v);
+            }
+            else if (ch == '2')
+            {
+                cout << "\n新性别：";
+                cin >> v;
+                current_user->setGender(v);
+            }
+            else if (ch == '3')
+            {
+                cout << "\n新手机号：";
+                cin >> v;
+                current_user->setPhone(v);
+            }
+            else if (ch == '4')
+            {
+                cout << "\n新邮箱：";
+                cin >> v;
+                current_user->setEmail(v);
+            }
+
+            else if (ch == '5')
+            {
+                cout << "\n原密码: ";
+                cin >> v;
+                if (v != current_user->getPassword())
+                {
+                    cout << "\n密码错误，按任意键返回...";
+                    _getch();
+                    page = Page::USER_INFO;
+                    continue;
+                }
+            }
+            else if (ch == 27)
+            {
+                page = Page::USER_INFO;
+                continue;
+            }
+
+            cout << "\n修改成功，按任意键返回...";
+            _getch();
+            page = Page::USER_INFO;
         }
 
         /* ================== 发布委托 ================== */
@@ -163,18 +242,45 @@ int main()
         {
             string n, t, l, c;
             double p;
-
             cout << "【发布委托】\n\n";
+
+            cout << "输入 q 可取消发布\n\n";
+
             cout << "标题：";
             cin >> n;
+            if (n == "q" || n == "Q")
+            {
+                page = Page::USER_INFO;
+                continue;
+            }
             cout << "时间：";
             cin >> t;
+            if (n == "q" || n == "Q")
+            {
+                page = Page::USER_INFO;
+                continue;
+            }
             cout << "地点：";
             cin >> l;
+            if (n == "q" || n == "Q")
+            {
+                page = Page::USER_INFO;
+                continue;
+            }
             cout << "内容：";
             cin >> c;
+            if (n == "q" || n == "Q")
+            {
+                page = Page::USER_INFO;
+                continue;
+            }
             cout << "报酬：";
             cin >> p;
+            if (n == "q" || n == "Q")
+            {
+                page = Page::USER_INFO;
+                continue;
+            }
 
             srv.dispatch(current_user->getID(), n, t, l, c, p);
 
@@ -187,17 +293,85 @@ int main()
         else if (page == Page::ENTRUST_DETAIL && current)
         {
             drawEntrustDetail(current);
-            cout << "\n[Enter] 接受委托  [ESC] 返回\n";
+            cout << "\n[Enter] 接受委托  [C] 评论  [ESC] 返回\n";
+        }
+
+        /*=================承接委托详情页=================*/
+        else if (page == Page::ACCEPT_DETAIL && current)
+        {
+            drawEntrustDetail(current);
+            cout << "\n[1] 完成委托";
+            cout << "\n[2] 委托失败";
+            cout << "\n[ESC] 返回\n";
         }
 
         /* ================== 键盘处理 ================== */
         char ch = _getch();
+
+        if (page == Page::ENTRUST_DETAIL && (ch == 'c' || ch == 'C'))
+        {
+            string comment;
+
+            cout << "\n输入评论: ";
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            getline(cin, comment);
+
+            if (!comment.empty())
+            {
+                entrustment *e = srv.find_entrustment(current->getId());
+                if (e)
+                {
+                    e->addComment(current_user->getName() + ": " + comment);
+                }
+            }
+        }
+
+        if (page == Page::ACCEPT_DETAIL)
+        {
+            if (ch == '1')
+            {
+                srv.complete_event(current_user->getID(), current->getId());
+                page = Page::MY_ACCEPT;
+                cursor = 0;
+            }
+            else if (ch == '2')
+            {
+                srv.finish_fail_event(current_user->getID(), current->getId());
+                page = Page::MY_ACCEPT;
+                cursor = 0;
+            }
+        }
+
+        if (page == Page::MY_DISPATCH && (ch == 'r' || ch == 'R'))
+        {
+            auto &list = current_user->accessDispatchHistory();
+            if (cursor < list.size())
+            {
+                bool ok = srv.cancel_dispatch(
+                    current_user->getID(),
+                    list[cursor].getId());
+
+                if (!ok)
+                {
+                    cout << "\n只能删除自己发布的委托!";
+                    _getch();
+                }
+
+                cursor = 0;
+            }
+        }
+
         if (page == Page::LOGIN)
         {
             if (ch == 'w')
                 loginCursor = max(0, loginCursor - 1);
             if (ch == 's')
                 loginCursor = min(2, loginCursor + 1);
+        }
+
+        if (ch == '1' && page == Page::USER_INFO)
+        {
+            page = Page::USER_EDIT;
         }
 
         if (ch == 27) // ESC
@@ -239,7 +413,7 @@ int main()
         {
             if (page == Page::LOGIN)
             {
-                if (loginCursor == 0) // 登录
+                if (loginCursor == 0) // 用户登录
                 {
                     clear();
                     cout << "====== 登录 ======\n\n";
@@ -262,7 +436,30 @@ int main()
                         _getch();
                     }
                 }
-                else if (loginCursor == 1) // 注册
+                else if (loginCursor == 1) // 管理员登录
+                {
+                    clear();
+                    cout << "====== 管理员登录 ======\n\n";
+                    cout << "手机号：";
+                    cin >> phone;
+                    cout << "密码：";
+                    cin >> password;
+
+                    current_user = srv.login(phone, password);
+                    if (current_user && current_user->isAdmin())
+                    {
+                        cout << "\n管理员登录成功!";
+                        _getch();
+                        page = Page::ENTRUST_LIST;
+                        cursor = 0;
+                    }
+                    else
+                    {
+                        cout << "\n管理员账号或密码错误!";
+                        _getch();
+                    }
+                }
+                else if (loginCursor == 2) // 注册
                 {
                     page = Page::REGISTER;
                 }
@@ -288,13 +485,35 @@ int main()
             }
             else if (page == Page::ENTRUST_DETAIL && current)
             {
-                srv.accept_clint(current_user->getID(), current->getId());
+                if (srv.accept_clint(current_user->getID(), current->getId()) == 1)
+                    cout << "\n接受成功, 按任意键继续...";
+                else if (srv.accept_clint(current_user->getID(), current->getId()) == 0)
+                    cout << "\n错误! 委托不存在, 按任意键继续...";
+                else if (srv.accept_clint(current_user->getID(), current->getId()) == -1)
+                    cout << "\n您的信誉分过低, 暂时无法接受新委托, 按任意键继续...";
+                _getch();
                 page = Page::MY_ACCEPT;
                 cursor = 0;
             }
+
             else if (page == Page::USER_INFO)
             {
                 page = Page::USER_PUBLISH;
+            }
+
+            else if (page == Page::USER_INFO)
+            {
+                page = Page::USER_PUBLISH;
+            }
+
+            else if (page == Page::MY_ACCEPT)
+            {
+                const auto &list = current_user->getAcceptHistory();
+                if (cursor < list.size())
+                {
+                    current = &list[cursor];
+                    page = Page::ACCEPT_DETAIL;
+                }
             }
         }
     }
